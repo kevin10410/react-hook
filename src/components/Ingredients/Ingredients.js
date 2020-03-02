8109c4b0f11219c8c83a6897ac49f2cf7c7f61b9
OTree <kevin10410@gmail.com>
2020-03-02 16:34:36 +0800

    refactor(Ingredient): implement addIngredientHandler with post request

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [ ingredients, setIngredients ] = useState([]);
+
+  const addIngredientHandler = ingredient => {
+    axios.post('/ingredient', ingredient)
+      .then(res => res.data)
+      .then(data => {
+        setIngredients(prev => [...prev, data]);
+      })
+      .catch(err => console.log(err));
+  };
+
+  const removeIngredientHandler = id => {
+    console.log(id);
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
-
+      <IngredientForm
+        addIngredient = { addIngredientHandler }
+      />
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList
+          ingredients = { ingredients }
+          removeIngredient = { removeIngredientHandler }
+        />
       </section>
     </div>
   );
