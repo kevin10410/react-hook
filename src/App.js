2b03e8dbe4b732e4af79406c5602f8c8681181c4
OTree <kevin10410@gmail.com>
2020-03-03 23:03:24 +0800

    refactor(App): implement isAuth context with useContext hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Ingredients from './components/Ingredients/Ingredients';
+import Auth from './components/Auth';
+import { ContextAuth } from '../src/context/contextAuth';
 
 const App = props => {
-  return <Ingredients />;
+  const contextAuth = useContext(ContextAuth);
+
+  return (
+    contextAuth.isAuth
+      ? <Ingredients />
+      : <Auth/>
+  )
 };
 
 export default App;
